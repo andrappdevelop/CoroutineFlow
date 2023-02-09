f5b724dbda497300f453855c3bac3339035135c5
IvanUsatov <andrappdevelop@gmail.com>
2023-02-09 19:23:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/coroutineflow/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.coroutineflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutineflow.databinding.ActivityMainBinding
+import com.example.coroutineflow.lesson.UsersActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonUsersActivity.setOnClickListener {
+            startActivity(UsersActivity.newIntent(this))
+        }
+    }
+}
